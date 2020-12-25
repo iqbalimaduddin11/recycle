424afe732ac3d980a776c0474f8d6a4ad3909b2f
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2020-12-25 15:53:43 +0700

    ubah database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('alamat');
-            $table->string('nomer');
-            $table->string('avatar');
+            $table->string('nomer')->nullable();
+            $table->string('avatar')->nullable();
             $table->integer('role')->default(1);
             $table->rememberToken();
             $table->timestamps();
