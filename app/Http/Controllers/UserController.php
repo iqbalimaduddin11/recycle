60ea4d5ea00a649798ab46d71b54e614f7e827cf
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2020-12-27 17:17:48 +0700

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,9 @@ class UserController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
+        
+        $user = User::where('email', $request->email);
+
 
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
