a95840d07d71b896e54dc46f2eee1c3e2c5665ac
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2020-12-30 17:29:49 +0700

    test

--- app/Http/Controllers/UserDetailController.php
+++ app/Http/Controllers/UserDetailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use App\UserDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserDetailController extends Controller
 {
@@ -16,77 +17,97 @@ class UserDetailController extends Controller
     public function getUserDetails($id)
     {
         $user = User::find($id);
-
+        
         if (!$user) {
             return $this->sendResponse('error', 'data tidak ditemukan', null, 404);
         }
-
+        
         $user = User::where('id', $id)->with('userdetail');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function setUserDetails(Request $request, UserDetail $userDetail)
     {
-        //
+        if (!User::find($request->user_id)) {
+            return $this->sendResponse('error', 'Data Tidak Ada', null, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'nomer' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response($validator->errors());
+        }
+
+        
+        try {
+            
+            $userDetail->user_id = $request->user_id;
+            $userDetail->nomer = $request->nomer;
+            $userDetail->alamat = $request->alamat;
+            if ($request->hasFile('avatar')) {
+                $image = $request->file('avatar');
+                $file = base64_encode(file_get_contents($image));
+
+                $client = new \GuzzleHttp\Client();
+                $response = $client->request('POST', 'https://freeimage.host/api/1/upload', [
+                    'form_params' => [
+                        'key' => '6d207e02198a847aa98d0a2a901485a5',
+                        'action' => 'upload',
+                        'source' => $file,
+                        'format' => 'json'
+                    ]
+                ]);
+
+                $data = $response->getBody()->getContents();
+                $data = json_decode($data);
+                $image = $data->image->url;
+
+                $userDetail->avatar = $image;
+            }
+
+            $userDetail->save();
+
+            $user = User::where('id',$request->$user_id)->with('userdetail')->first();
+
+            return $this->sendResponse('succsess', 'Data Berhasil ditambah', compact('user'), 201);
+
+        } catch (\Exception $th) {
+            
+            return $this->sendResponse('error', 'Data Gagal ditambah', $th->getMessage(), 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function updateUser($id) {
+
+        $user = User::find($id);
+
+        if (!$user) {
+            return $this->sendResponse('error', 'Data Tidak ditemukan', null ,404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'string',
+            'email' => 'email|unique:users,email,'.$user->id,
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $data = $request->all();
+
+        $result = array_filter($data);
+
+        try {
+            $user->update($result);
+
+            return $this->sendResponse('success', 'Data Berhasil diupdate', compact('user'), 201);
+        } catch (\Exception $th) {
+            return $this->sendResponse('error', 'Data Gagal diupdate', null, 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\UserDetail  $userDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function show(UserDetail $userDetail)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\UserDetail  $userDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(UserDetail $userDetail)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\UserDetail  $userDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, UserDetail $userDetail)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\UserDetail  $userDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(UserDetail $userDetail)
-    {
-        //
-    }
 }
--- app/UserDetail.php
+++ app/UserDetail.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDetail extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'nomer', 'avatar', 'alamat'
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    public function user() {
+
+        return $this->belongsTo('App/user', 'user_id', 'id');
+    }
 }
