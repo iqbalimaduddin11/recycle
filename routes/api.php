53bb04d8692d9092f29ce48b3e9338bbb03ffc5f
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2021-01-14 10:49:33 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::group(['middleware' => ['jwt.verify']], function () {
     
     //penjemputan sampah
     Route::get('/penjemputan/{id}', 'PenjemputanController@getPenjemputan');
-    Route::post('/penjemputan/create/{id}', 'PenjemputanController@createPenjemputan');
+    Route::post('/penjemputan//create', 'PenjemputanController@createPenjemputan');
     Route::post('/penjemputan/update/{id}', 'PenjemputanController@setStatusPenjemputan');
 });
 
