16d906919177cc042165805e0c384ae8d4adb6fb
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2021-01-20 13:58:17 +0700

    test

--- app/Http/Controllers/PenjemputanController.php
+++ app/Http/Controllers/PenjemputanController.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\Validator;
 
 class PenjemputanController extends Controller
 {
-    public function getPenjemputan($id)
+    public function getPenjemputan()
     {
-        $penjemputan = Penjemputan::where('id', $id)->with('user')->get();
+        $penjemputan = Penjemputan::get();
 
         return $this->sendResponse('success', 'Data Berhasil diambil', $penjemputan, 200);
 
@@ -36,47 +36,16 @@ class PenjemputanController extends Controller
             $penjemputan->nomer = $request->nomer;
             $penjemputan->keterangan = $request->keterangan;
             $penjemputan->alamat = $request->alamat;
-            // $penjemputan->status = $request->status;
 
             $penjemputan->save();
 
-            return $this->sendResponse('succsess', 'Data Berhasil ditambah', compact('penjemputan'), 201);
+            return $this->sendResponse('succsess', 'Data Berhasil ditambah', $penjemputan, 201);
 
         } catch (\Exception $th) {
             
             return $this->sendResponse('error', 'Data Gagal ditambah', $th->getMessage(), 500);
         }
     }
-
-    // public function addPenjemputan(Request $request,  Penjemputan $penjemputan)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'string',
-    //         'nomer' => 'string',
-    //         'keterangan' => 'string',
-    //         'alamat' => 'string',
-    //         'status' => '0'
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response($validator->errors());
-    //     }
-
-    //     $penjemputan->name = $request->name;
-    //     $penjemputan->nomer = $request->nomer;
-    //     $penjemputan->keterangan = $request->keterangan;
-    //     $penjemputan->alamat = $request->alamat;
-    //     $penjemputan->status = $request->status;
-
-        
-    //     try {
-    //         $penjemputan->save();
-
-    //         return $this->sendResponse('success', 'Menunggu Konfirmasi', $penjemputan, 200);
-    //     } catch (\Exception $th) {
-    //         return $this->sendResponse('error', 'Gagak Menunggu Konfirmasi', $th->getMessage(), 500);
-    //     }
-    // }
 
     public function setStatusPenjemputan(Request $request, $id)
     {
